039f3a71083b78213473d17b6b75bdce72ce831b
cyberfolk <andrea1995ferrari.com>
2023-04-19 20:42:38 +0200

    Make it usable for movies and for series

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -6,29 +6,22 @@ export default {
     ProductItem,
   },
   props: {
-    movies: Array,
-    series: Array,
+    products: Array,
+  },
+  methods: {
+    title(p) {
+      return p.title ? p.title : p.name;
+    },
+    original_title(p) {
+      return p.original_title ? p.original_title : p.original_name;
+    },
   },
 };
 </script>
 <template>
   <div class="row">
-    <div class="col-6">
-      <h2>Movies</h2>
-      <ul v-if="movies">
-        <li v-for="movie in movies">
-          <ProductItem :title="movie.title" :original="movie.original_title" :lang="movie.original_language" :vote="movie.vote_average" :poster="movie.poster_path" />
-        </li>
-      </ul>
-    </div>
-    <!-- /.col-6 -->
-    <div class="col-6">
-      <h2>Series</h2>
-      <ul v-if="series">
-        <li v-for="serie in series">
-          <ProductItem :title="serie.name" :original="serie.original_name" :lang="serie.original_language" :vote="serie.vote_average" :poster="serie.poster_path" />
-        </li>
-      </ul>
+    <div class="col-6" v-for="p in products">
+      <ProductItem :title="title(p)" :original="original_title(p)" :lang="p.original_language" :vote="p.vote_average" :poster="p.poster_path" />
     </div>
     <!-- /.col-6 -->
   </div>
