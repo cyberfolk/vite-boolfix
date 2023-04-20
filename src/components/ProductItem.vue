dda7a0dd6b79ae802fa8ec32ec8a1325f4cf67b4
cyberfolk <andrea1995ferrari.com>
2023-04-20 10:27:49 +0200

    Show poster and on hover show info

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -18,6 +18,7 @@ export default {
       return !lang_db.notSupported.includes(lang);
     },
     srcPath(endPath) {
+      console.log(endPath);
       return `https://image.tmdb.org/t/p/w342/${endPath}`;
     },
     fillStars(vote) {
@@ -31,16 +32,15 @@ export default {
 };
 </script>
 <template>
-  <div class="card h-100">
-    <img :src="srcPath(poster)" class="card-img-top" :alt="title" />
-    <div class="card-body">
+  <div class="card">
+    <img v-if="poster" :src="srcPath(poster)" class="card-img" :alt="title" />
+    <div class="card-body h-100 w-100">
       <h5 class="card-title">{{ title }}</h5>
       <p class="card-text">{{ original }}</p>
       <div>
         <span v-if="supported(lang)"><LangFlag :iso="lang" /></span>
         <span v-else>{{ lang }} - not supported</span>
       </div>
-      <div>{{ vote }}</div>
       <span v-for="n in fillStars(vote)" class="fa-solid fa-star"></span>
       <span v-for="n in emptyStars(vote)" class="fa-regular fa-star"></span>
     </div>
@@ -48,3 +48,25 @@ export default {
   </div>
   <!-- /.card -->
 </template>
+
+<style lang="scss" scoped>
+.card {
+  position: relative;
+  height: 437px;
+}
+
+.card-img {
+  position: relative;
+  z-index: 2;
+  max-height: 100%;
+}
+.card-body {
+  position: absolute;
+  background-color: white;
+  z-index: 1;
+}
+
+.card:hover .card-img {
+  display: none;
+}
+</style>
