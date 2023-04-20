b329cad4284d170517ffaa1564786036d2b1ce08
cyberfolk <andrea1995ferrari.com>
2023-04-20 10:26:42 +0200

    Move SearchBox from main to header

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,11 +1,23 @@
 <script>
+import { state } from "../state.js";
+import SearchBox from "./SearchBox.vue";
 export default {
   name: "SiteHeader",
-  components: {},
+  components: {
+    SearchBox,
+  },
+  data() {
+    return {
+      state,
+    };
+  },
 };
 </script>
 
 <template>
-  <header><h1>Boolfix</h1></header>
+  <header class="d-flex justify-content-between align-items-center">
+    <h1>Boolfix</h1>
+    <SearchBox @searchMovie="state.fetchMovies(state.API_URL_MOVIE, state.filter), state.fetchSeries(state.API_URL_SERIE, state.filter)" />
+  </header>
 </template>
 <style lang="scss" scoped></style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,11 +1,9 @@
 <script>
 import { state } from "../state.js";
-import SearchBox from "./SearchBox.vue";
 import ProductList from "./ProductList.vue";
 export default {
   name: "SiteMain",
   components: {
-    SearchBox,
     ProductList,
   },
   data() {
@@ -18,7 +16,6 @@ export default {
 
 <template>
   <main>
-    <SearchBox @searchMovie="state.fetchMovies(state.API_URL_MOVIE, state.filter), state.fetchSeries(state.API_URL_SERIE, state.filter)" />
     <div class="row">
       <div class="col-6 border bg-primary">
         <div class="ms_movies px-3">
